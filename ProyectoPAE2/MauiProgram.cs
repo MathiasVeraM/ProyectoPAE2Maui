51a3f12c5b724e3a86fc0d3e23fe5dddd59fc940
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-12-09 15:17:04 -0500

    Se modifico el archivo styles.xaml para incluir el estilo Poppins como tipo de letra regular en el sistema, para usarles se descargo y agrego el ttf de Poppins Regular y Bold añadidos en Resources Fonts.

--- ProyectoPAE2/MauiProgram.cs
+++ ProyectoPAE2/MauiProgram.cs
@@ -13,6 +13,8 @@ namespace ProyectoPAE2
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
+                    fonts.AddFont("Poppins-Bold.ttf", "PoppinsBold");
                 });
 
 #if DEBUG
