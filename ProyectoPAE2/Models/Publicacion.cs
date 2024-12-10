cdf5b799b34c99fabaeb33cd23a2342a6bab28dc
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-12-10 00:39:56 -0500

    Se creo el modelo Publicacion, para llenar los datos de la mascota.

---
+++ ProyectoPAE2/Models/Publicacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPAE2.Models
+{
+    public class Publicacion
+    {
+        public int Id { get; set; }
+        public string NombreMascota { get; set; }
+        public string Raza { get; set; }
+        public string Sexo { get; set; }
+        public int Edad { get; set; }
+        public float Peso { get; set; }
+        public string Tamaño { get; set; }
+        public bool Esterilizacion { get; set; }
+        public string Enfermedades { get; set; }
+        public string Comportamiento { get; set; }
+    }
+}
