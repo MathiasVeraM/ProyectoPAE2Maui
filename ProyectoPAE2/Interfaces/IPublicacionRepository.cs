37b6e3587ad771684347e598914fa9e96d1012da
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-12-10 00:45:57 -0500

    Se agrego la interfaz y el repositorio para guardar datos en archivos del modelo Publicacion

---
+++ ProyectoPAE2/Interfaces/IPublicacionRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProyectoPAE2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPAE2.Interfaces
+{
+    public interface IPublicacionRepository
+    {
+        bool CrearPublicacion(Publicacion publicacion);
+        bool ActualizarPublicacion(Publicacion publicacion);
+        bool EliminarPublicacion(int id);
+        Publicacion DevuelveInfoPublicacion(int id);
+    }
+}
---
+++ ProyectoPAE2/Repositories/PublicacionFileRepository.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using ProyectoPAE2.Interfaces;
+using ProyectoPAE2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPAE2.Repositories
+{
+    public class PublicacionFileRepository : IPublicacionRepository
+    {
+        public string _fileName = Path.Combine(FileSystem.AppDataDirectory, "publicaciones.txt");
+        public bool ActualizarPublicacion(Publicacion publicacion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CrearPublicacion(Publicacion publicacion)
+        {
+            try
+            {
+                string json_data = JsonConvert.SerializeObject(publicacion);
+                File.WriteAllText(_fileName, json_data);
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Publicacion DevuelveInfoPublicacion(int id)
+        {
+            Publicacion publicacion = new Publicacion();
+            try
+            {
+                if (File.Exists(_fileName))
+                {
+                    string json_data = File.ReadAllText(_fileName);
+                    publicacion = JsonConvert.DeserializeObject<Publicacion>(json_data);
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return publicacion;
+        }
+
+        public bool EliminarPublicacion(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
