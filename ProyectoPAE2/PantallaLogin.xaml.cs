32314ddce1aa10c3cdaa21ffaecadcc19b8359cd
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-12-09 22:54:06 -0500

    Primer intento de realizar una busqueda en el archivo para iniciar sesion con usuario y contraseña. Parece que solo sirve con un solo usuario, vamos a modificar eso.

--- ProyectoPAE2/PantallaLogin.xaml.cs
+++ ProyectoPAE2/PantallaLogin.xaml.cs
@@ -1,15 +1,53 @@
 
+using ProyectoPAE2.Models;
+using ProyectoPAE2.Repositories;
+
 namespace ProyectoPAE2;
 
 public partial class PantallaLogin : ContentPage
 {
-	public PantallaLogin()
+    private UsuarioFilesRepository _usuarioRepository;
+    public PantallaLogin()
 	{
 		InitializeComponent();
+		_usuarioRepository = new UsuarioFilesRepository();
 	}
 
 	private async void OnRegisterTapped(object sender, EventArgs e)
 	{
         await Navigation.PushAsync(new PantallaRegistro());
+    }
+
+    private async void BotonLogin_Clicked(object sender, EventArgs e)
+    {
+        string correo = editor_correo.Text;
+        string clave = editor_contraseña.Text;
+
+        if(string.IsNullOrEmpty(correo) && string.IsNullOrEmpty(clave))
+        {
+            await DisplayAlert("Error", "Deben ingresarse datos para iniciar sesión", "OK");
+            return;
+        }
+
+        try
+        {
+            Usuario usuario = _usuarioRepository.DevuelveInfoUsuario(1);
+            if (usuario == null) {
+                await DisplayAlert("Error", "No se encontró ningún usuario registrado.", "OK");
+                return;
+            }
+
+            if (usuario.Correo == correo && usuario.Clave == clave)
+            {
+                await Navigation.PushAsync(new MainPage());
+            } else {
+                await DisplayAlert("Error", "La clave o correo son incorrectos", "OK");
+                return;
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
     }
 }
